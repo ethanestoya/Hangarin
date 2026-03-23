050009ca27871b177eecc40f9d4ecf617c454405
ethanestoya <work.ethanestoya@gmail.com>
2026-03-23 23:46:47 +0800

    Modified the Menu/Nagivation Bar

--- hangarin/taskmanager/views.py
+++ hangarin/taskmanager/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from taskmanager.models import Category, Priority, Task, Note, SubTask
 
 # Create your views here.
+# List View
+class HomePageView(ListView):
+    model = Category
+    context_object_name = 'home'
+    template_name = 'home.html'
+
